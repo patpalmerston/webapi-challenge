4c675d91fb324f2a51fa595efba0e2090c7411bf
patpalmerston <patpalmerston@gmail.com>
2019-05-03 09:46:48 -0700

    get by id up on projects and actions

--- data/routers/actions-router.js
+++ data/routers/actions-router.js
@@ -12,7 +12,19 @@ router.get('/', async (req, res) => {
   try {
     const actions = await actionDb.get();
     res.status(201).json(actions)
-  } catch (err) {sendUserError(500, 'Poject with that ID does not exist', err)}
+  } catch (err) {sendUserError(500, 'Project with that ID does not exist', err)}
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const action = await actionDb.get(req.params.id);
+
+    if (action) {
+      res.status(200).json(action);
+    } else {
+      res.status(404).json({ error: 'The project info could not be retrieved', err })
+    }
+  } catch (err) {sendUserError(500, 'The post info could not be retrieved.', err)}
 });
 
 module.exports = router;
--- data/routers/projects-router.js
+++ data/routers/projects-router.js
@@ -15,5 +15,16 @@ router.get('/', async (req, res) => {
   } catch (err) {sendUserError(500, 'Poject with that ID does not exist', err)}
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await projDb.get(req.params.id);
+
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ error: 'The project info could not be retrieved', err })
+    }
+  } catch (err) {sendUserError(500, 'The post info could not be retrieved.', err)}
+})
 
 module.exports = router;
